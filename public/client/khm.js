8ab5c13e687f37d9fdfcd6a3f0bf8c703b5ad438
danny50167 <danny.rollingsky@gmail.com>
2022-11-27 17:30:06 +0900

    221127

---
+++ public/client/khm.js
@@ -0,0 +1,10 @@
+const body = document.querySelector("body");
+const btn_newDiary = document.getElementById("btn_newDiary");
+
+const createDiaryPopup = (e) => {
+  const popup = document.createElement("div");
+  popup.innerHTML = 
+  body.appendChild()
+}
+
+btn_newDiary.addEventListener("click", createDiaryPopup);
---
+++ server.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req,res) => {
+  res.render("index");
+});
+
+app.get("/khm", (req,res) => {
+  res.render("khm");
+})
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000\n-> http://localhost:3000 <-");
+});
